8dda7f8cc1b3a0e558ec867f31871cf08083e23c
onepierre <onepierrethe4@gmail.com>
2020-11-22 18:49:52 +0100

    begin to create a manual correction function

--- face_detection.py
+++ face_detection.py
@@ -4,12 +4,11 @@ import cv2
 import face_recognition
 from pyvis.network import Network
 import pickle
-import networkx as nx
-
-import numpy as np
 
 
 def nameTransform(name):
+    if name == "Unknown":
+        return name
     rep = name[:-4]
     rep = rep.replace(".","_")
     return rep
@@ -41,41 +40,49 @@ def trombi():
 def detecte_visages():
 
     # Charge les embeddings des visages connus
-    with open('saves\\net.trombi', 'rb') as input:
-        net = pickle.load(input)
-    with open('saves\\known_face_encodings.txt', 'rb') as input:
-        known_face_encodings = pickle.load(input)
-    with open('saves\\known_face_names.txt', 'rb') as input:
-        known_face_names = pickle.load(input)
+    with open('saves\\net.trombi', 'rb') as entree:
+        net = pickle.load(entree)
+    with open('saves\\known_face_encodings.txt', 'rb') as entree:
+        known_face_encodings = pickle.load(entree)
+    with open('saves\\known_face_names.txt', 'rb') as entree:
+        known_face_names = pickle.load(entree)
 
     #cherche les visages sur chaque photo AP
     for image in os.listdir('photo_AP'):
+
         # on charge l'image en mémoire
         img = face_recognition.load_image_file('photo_AP' + '\\' +image)
         image_out = 'AP_labelled' + '\\' + "labelled_"+ image
-
         # Trouve les visages sur la photo
         face_locations = face_recognition.face_locations(img)
         face_encodings = face_recognition.face_encodings(img, face_locations)
-
-
         face_names = []
+        face_distances = []
+
+        # Cherche à identifier chaque visage
         for face_encoding in face_encodings:
             # See if the face is a match for the known face(s)
             matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
             name = "Unknown"
 
-            # # If a match was found in known_face_encodings, just use the first one.
             if True in matches:
                 first_match_index = matches.index(True)
                 name = known_face_names[first_match_index]
-            # else:
-            #     face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
-            #     best_match_index = np.argmin(face_distances)
-            #     name = known_face_names[best_match_index] + '(guess)'
+
+            face_distance = face_recognition.face_distance(known_face_encodings, face_encoding)
             face_names.append(name)
+            face_distances.append(face_distance)
 
+        with open('saves\\Recognition\\' + nameTransform(image) +'face_distances', 'wb') as output:
+            pickle.dump(face_distances, output, pickle.HIGHEST_PROTOCOL)
+        with open('saves\\Recognition\\' + nameTransform(image) + 'face_names', 'wb') as output:
+            pickle.dump(face_names, output, pickle.HIGHEST_PROTOCOL)
+        with open('saves\\Recognition\\' + nameTransform(image) + 'face_encodings', 'wb') as output:
+            pickle.dump(face_encodings, output, pickle.HIGHEST_PROTOCOL)
+        with open('saves\\Recognition\\' + nameTransform(image) + 'face_locations', 'wb') as output:
+            pickle.dump(face_locations, output, pickle.HIGHEST_PROTOCOL)
 
+        # add nodes to the graph
         for n1 in face_names:
             for n2 in face_names:
                 if n1 != "Unknown" and n2 != "Unknown" and n1 != n2:
@@ -86,7 +93,7 @@ def detecte_visages():
             for j in range(len(img[i])):
                 img[i][j][0],img[i][j][2] = img[i][j][2],img[i][j][0]
 
-
+        # draw the rectangles on the picture
         for i in range(len(face_locations)):
             top, right, bottom, left = face_locations[i]
             name = face_names[i]
@@ -106,10 +113,60 @@ def detecte_visages():
     for node in net.nodes:
         node["title"] += " neighbors:<br>" + "<br>".join(neighbor_map[node["id"]])
         node["value"] = 1
+
+    with open('saves\\net.trombi', 'wb') as output:
+        pickle.dump(net, output, pickle.HIGHEST_PROTOCOL)
+
     net.show("graphe.html")
+
+def nameCorrection():
+    for image in os.listdir('photo_AP'):
+        with open('saves\\Recognition\\' + nameTransform(image) + 'face_distances', 'rb') as entree:
+            face_distances = pickle.load(entree)
+        with open('saves\\Recognition\\' + nameTransform(image) + 'face_names', 'rb') as entree:
+            face_names = pickle.load(entree)
+        with open('saves\\Recognition\\' + nameTransform(image) + 'face_locations', 'rb') as entree:
+            face_locations= pickle.load(entree)
+
+
+        for i in range(len(face_locations)):
+            img = face_recognition.load_image_file('photo_AP' + '\\' + image)
+            top, right, bottom, left = face_locations[i]
+            name = face_names[i]
+            # Draw a box around the face
+            cv2.rectangle(img, (left, top), (right, bottom), (0, 0, 255), 2)
+            # Draw a label with a name below the face
+            cv2.rectangle(img, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(img, nameTransform(name), (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+            # Inverse les Bleus et Rouges pour passer du RGB au BGR (pour fonctionner avec cv2)
+            for i in range(len(img)):
+                for j in range(len(img[i])):
+                    img[i][j][0], img[i][j][2] = img[i][j][2], img[i][j][0]
+
+            print("Le nom est-il correct? (n for non)")
+            cv2.namedWindow("visage", cv2.WINDOW_NORMAL)
+            cv2.imshow("visage", img)
+            key = cv2.waitKey(5000000)
+            cv2.destroyWindow("visage")
+            print(key)
+
+
+
+            cv2.destroyWindow("visage")
+            if key == 110:
+                for i in range(len(face_names)):
+                    print(face_names[i] + face_distances[i])
+                    nom = input("Entrez le nom de la personne (avec un . à la place de l'espace")
+                    print(nom)
+
+
 
 if __name__ == "__main__":
     # applique
-    trombi()
-    print("Trombinoscope saved")
-    detecte_visages()
+    # trombi()
+    # print("Trombinoscope saved")
+    # detecte_visages()
+    # print("detect visage terminé")
+    nameCorrection()
